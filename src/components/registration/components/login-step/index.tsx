c9cb5fa8815bcf41ef35793bd47ec5366fb75e5e
mukul-kmr-jha <jimickeyji@gmail.com>
2022-08-27 13:48:40 +0530

    complete login UI

--- src/components/registration/components/login-step/index.tsx
+++ src/components/registration/components/login-step/index.tsx
@@ -1,24 +1,60 @@
-import {useContext, useState} from "react";
+import {ChangeEventHandler, SetStateAction, useContext, useState} from "react";
 import {AppContext} from "../../../../contexts/app.context";
 import {IStore} from "../../../../interfaces/user-reducer.interface";
 import './index.scss';
 
 export const LoginStepComponent = () => {
-    const { state, dispatch } = useContext(AppContext) as IStore;
+    const {state, dispatch} = useContext(AppContext) as IStore;
 
     const [email, setEmail] = useState(state.email);
 
+    const handleGoogleOAuth = () => {
+        console.log('Google OAuth');
+    }
+
+    const handleManualSignin = () => {
+        console.log('Manual Sign in');
+    }
+
     return (
         <div className='login-component'>
-            <div className="content">
+            <div className="container">
                 <div className='header'>
                     <span className='header-label'>
                         <span>Sign document using</span>
-                        <p>{email}</p>
+                        <p>Sanket@digio.in</p>
                     </span>
                     <span className='img-cont'>
                         <img src="./company-logo.png" alt="logo"/>
                     </span>
+                </div>
+                <div className='content'>
+                    <div className='oauth'>
+                       <span className='oauth-info-cont'>
+                            <p>Sanket@digio.in you use Gmail?</p>
+                            <p>login using google</p>
+                       </span>
+                        <span className='oauth-btn'>
+                            <button onClick={handleGoogleOAuth}>Google</button>
+                        </span>
+                    </div>
+                    <div className='section-separator'>
+                        <span>OR</span>
+                    </div>
+                    <div className='manual'>
+                        <div className='input-box-cont'>
+                            <span>Proceed with your email address</span>
+                            <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <div className="terms">
+                            By Continuing, I confirm to the <b>Terms and Service</b> and <b>Privacy Policy</b> of
+                            <a href="https://digio.in">Digio.in</a>
+                        </div>
+                        <div className="manual-btn">
+                            <button onClick={handleManualSignin}>continue</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
